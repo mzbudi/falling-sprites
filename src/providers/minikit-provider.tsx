0f29b7148cf1f973cc703f3bb65031ac8ade8c71
mzbudi <ronibudianto361@gmail.com>
2025-08-27 13:29:44 +0700

    fix provider

--- src/providers/minikit-provider.tsx
+++ src/providers/minikit-provider.tsx
@@ -1,10 +1,17 @@
 import { MiniKit } from "@worldcoin/minikit-js";
 import { type ReactNode, useEffect } from "react";
+import { useWalletStore } from "../store/useWalletStore";
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     MiniKit.install();
     console.log(MiniKit.isInstalled());
+    if (MiniKit.isInstalled()) {
+      console.log("MiniKit is installed");
+      useWalletStore
+        .getState()
+        .setWalletInfo("world-app-user", null, null, "world");
+    }
   }, []);
 
   return <>{children}</>;
